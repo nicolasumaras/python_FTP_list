ed6f1816a23114d85fb7d3ad981e6e5de925f0d4
nicolasumaras <umarascom@gmail.com>
2021-01-03 15:16:30 -0300

    V1.1

--- FTP_list.py
+++ FTP_list.py
@@ -1,8 +1,7 @@
 import ftplib
-
+import sys
 import os
-
-
+import io
 
 #------------constants------------------
 
@@ -76,43 +75,51 @@ def append(text, ):
 
     f.close()
 
+  
 
+def list_all_ftp_files(ftp, dir):
+        print("hey")
+        
+        stream = io.StringIO()
+        sys.stdout = stream
+        print("hey")
+        ftp.nlst(dir)
+        streamed_result = stream.getvalue()
+        non_dirs = []
+        dirs = []
+        reduced = [x for x in streamed_result.split(' ') if x != '']
+        reduced = [x.split('\n')[0] for x in reduced]
+        indexes = [ix + 1 for ix,x in enumerate(reduced) if x == '<DIR>']
+        folders = [reduced[ix] for ix in indexes]
+        #files = ftp.mlsd(sub_dir)
+        if dir == '/':
+            non_folders = [x for x in ftp.nlst() if x not in folders]
+        else:
+            non_folders = [x for x in ftp.nlst(dir) if x not in folders]
+            non_folders = [dir + '/' + x for x in non_folders]
+            folders = [dir + '/' + x for x in folders]
+          
+        if dirs == []:
+            dirs.extend(folders)
+        if non_dirs == []:
+            non_dirs.extend(non_folders)
+  
+        if len(folders) > 0:
+            for sub_folder in sorted(folders):
+                result = list_all_ftp_files(ftp, sub_folder)
+                dirs.extend(result[0])
+                non_dirs.extend(result[1])
+        return non_dirs          
 
 def process():
-
-    clear_file_report()
-
-    
-
     files = []
-
-
-
-    try:
-
-        files = ftp.mlsd(sub_dir)
-
-            
-
-    except ftplib.error_perm as resp:
-
-        if str(resp) == "550 No files found":
-
-            append("")
-
-        else:
-
-            append(resp)
-
-            
-
+    clear_file_report()
+    files = list_all_ftp_files(ftp , sub_dir)
+    
     for f in files:
 
-        print("Writing:%s" % f)
-
-        append(f)
-
-
+       print("Writing:%s" % f)
+       append(f)
 
 if __name__ == "__main__":
 
